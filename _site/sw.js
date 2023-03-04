0138c145648e357ccb2bbc7b9ed7a5ba1d1c4b02
milhaud1201 <milhaud1201@gmail.com>
2023-03-05 01:40:42 +0900

    delete category

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230227.162908';
+const cacheName = 'chirpy-20230305.013958';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
